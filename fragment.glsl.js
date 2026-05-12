dae27dfb17cd33d8d1f78f8387ea8c89626fa857
JJonKang <jjonkang03@gmail.com>
2026-05-12 00:22:13 -0500

    Initial commit

---
+++ fragment.glsl.js
@@ -0,0 +1,10 @@
+export default `#version 300 es
+precision mediump float;
+
+in vec4 vColor;
+out vec4 outColor;
+
+void main() {
+  outColor = vColor;
+}
+`;
---
+++ main.js
@@ -0,0 +1,2 @@
+import vertexShaderSrc from 'vertex.glsl.js';
+import fragmentShaderSrc from 'fragment.glsl.js';
