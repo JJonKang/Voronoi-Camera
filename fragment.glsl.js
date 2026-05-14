775809791b7ff467e9068f969094b8d77c618306
JJonKang <jjonkang03@gmail.com>
2026-05-14 13:49:46 -0500

    Value cleaning

--- fragment.glsl.js
+++ fragment.glsl.js
@@ -75,18 +75,17 @@ void main() {
   }
 
   vec4 cam = texture(texture0, uv + vec2(m_dist) * 0.0215);
-  float brightnessScale = length(color) / pow(1.1, 0.5) * brightness;
-  //change the 2nd element in vec2(0.3, __) for how dark each cell should be
-  if(length(color) > light4 * pow(3.0, 0.5)){
+  float brightnessScale = length(color) / 1.05 * brightness;
+  if(length(color) > light4 * 1.7){
     fragColor = vec4(cam.rgb * dot(m_point, vec2(0.17, 0.3)) * brightnessScale, 1.0);
   }
-  else if(length(color) > light3 * pow(3.0, 0.5)){
+  else if(length(color) > light3 * 1.7){
     fragColor = vec4(cam.rgb * dot(m_point, vec2(0.20, 0.4)) * brightnessScale, 1.0);
   }
-  else if(length(color) > light2 * pow(3.0, 0.5)){
+  else if(length(color) > light2 * 1.7){
     fragColor = vec4(cam.rgb * dot(m_point, vec2(0.23, 0.5)) * brightnessScale, 1.0);
   }
-  else if(length(color) > light1 * pow(3.0, 0.5)){
+  else if(length(color) > light1 * 1.7){
     fragColor = vec4(cam.rgb * dot(m_point, vec2(0.26, 0.6)) * brightnessScale, 1.0);
   }
   else{
